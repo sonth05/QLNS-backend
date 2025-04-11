d8e46b406889204a363b823744475dd5808de150
sonth05 <sonnguyenthanh1205@gmail.com>
2025-04-11 14:15:13 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,24 @@
 
+// db.js
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.MONGO_URI;
+const options = {};
+
+let client;
+let clientPromise;
+
+if (!process.env.MONGO_URI) {
+  throw new Error("Please add your Mongo URI to .env");
+}
+
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri, options);
+  global._mongoClientPromise = client.connect();
+}
+clientPromise = global._mongoClientPromise;
+
+export async function connectToDB() {
+  const client = await clientPromise;
+  return client.db(); // mặc định DB
+}
